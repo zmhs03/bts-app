e8bef2bb4f9240ace4de78905b983a94acf372eb
zmhs03 <zamahayise22@gmail.com>
2024-06-26 03:24:48 +0200

    commit 3

--- bts-app/src/components/banner.js
+++ bts-app/src/components/banner.js
@@ -12,55 +12,70 @@ import Banner7 from "../assets/suga.jpg";
 import Banner8 from "../assets/tae.jpg";
 import "../styles/homeBanner.css";
 
-const banner = () => {
+const Banner = () => {
   const banners = [
     {
       image: Banner1,
       title: "BTS",
       description:
         "BTS, also known as the Bangtan Boys, is a South Korean boy band formed by Big Hit Entertainment. Consisting of seven members: RM, Jin, Suga, J-Hope, Jimin, V, and Jungkook.",
+      spotifyLink: "https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX", // BTS Spotify link
     },
     {
       image: Banner2,
       title: "J-Hope",
       description:
         "Jung Ho-seok (정호석), better known as J-Hope, is a rapper, dancer, and songwriter in BTS. He has also released solo works, including the mixtape 'Hope World'.",
+      spotifyLink:
+        "https://open.spotify.com/artist/0b1sIQumIAsNbqAoIClSpy?si=u5CBBpMpTEumGuJ_kpU4wA", // J-Hope Spotify link
     },
     {
       image: Banner3,
       title: "Jimin",
       description:
         "Park Ji-min (박지민), known mononymously as Jimin, is a vocalist and dancer in BTS. He is celebrated for his powerful vocal range and dynamic dance performances.",
+      spotifyLink:
+        "https://open.spotify.com/artist/1oSPZhvZMIrWW5I41kPkkY?si=49a7038147884b48", // Jimin Spotify link
     },
     {
       image: Banner4,
       title: "Jin",
       description:
         "Kim Seok-jin (김석진), known as Jin, is a vocalist and visual of BTS. He has contributed to BTS's discography with his solo song 'Epiphany' among others.",
+      spotifyLink:
+        "https://open.spotify.com/artist/5vV3bFXnN6D6N3Nj4xRvaV?si=5d64968ac33b45ef", // Jin Spotify link
     },
     {
       image: Banner5,
       title: "Jungkook",
       description:
         "Jeon Jung-kook (전정국), known as Jungkook, is the youngest member and main vocalist of BTS. His solo works include 'Euphoria' and 'My Time'.",
+      spotifyLink:
+        "https://open.spotify.com/artist/6HaGTQPmzraVmaVxvz6EUc?si=FRsCRS0dR7G0VPabVWCcMg", // Jungkook Spotify link
     },
     {
       image: Banner6,
       title: "RM",
       description:
         "Kim Nam-joon (김남준), known as RM, is the leader and main rapper of BTS. He has released solo works such as the mixtapes 'RM' and 'Mono'.",
+      spotifyLink:
+        "https://open.spotify.com/artist/2auC28zjQyVTsiZKNgPRGs?si=_29dlguKQG2Q_iiY3oGbbQ", // RM Spotify link
     },
     {
       image: Banner7,
       title: "Suga",
       description:
-        "Min Yoon-gi (민윤기), known as Suga, is a rapper, songwriter, and producer in BTS. He has released solo works including the mixtape 'Agust D'.",
+        "Min Yoon-gi (민윤기), known as Suga or Agust D, is a rapper, songwriter, and producer in BTS. He has released solo works including the mixtape 'Agust D'.",
+      spotifyLink:
+        "https://open.spotify.com/artist/5RmQ8k4l3HZ8JoPb4mNsML?si=cyIVd7wBRNSohRIQqGV9vA", // Suga Spotify link
     },
     {
       image: Banner8,
       title: "V",
       description:
         "Kim Tae-hyung (김태형), known as V, is a vocalist in BTS. He has contributed to BTS's sound with his solo songs like 'Singularity' and 'Stigma'.",
+      spotifyLink:
+        "https://open.spotify.com/artist/3JsHnjpbhX4SnySpvpa9DK?si=3d467ab9c424459a", // V Spotify link
     },
   ];
 
@@ -74,68 +89,49 @@ const banner = () => {
     autoplaySpeed: 3000,
   };
 
-  return React.createElement(
-    "div",
-    { className: "banner" },
-    React.createElement(
-      Slider,
-      Object.assign({}, settings),
-      banners.map((banner, index) =>
-        React.createElement(
-          "div",
-          { key: index, className: "banner__slide" },
-          React.createElement("img", {
-            src: banner.image,
-            alt: banner.title,
-            className: "banner__image",
-          }),
-          React.createElement(
-            "div",
-            { className: "banner__contents" },
-            React.createElement(
-              "h1",
-              { className: "banner__title" },
-              banner.title
-            ),
-            React.createElement(
-              "div",
-              { className: "banner__buttons" },
-              React.createElement(
-                "button",
-                {
-                  className: "banner__button",
-                  onClick: () =>
+  const openSpotifyLink = (link) => {
+    window.open(link, "_blank");
+  };
+
+  return (
+    <div className="banner">
+      <Slider {...settings}>
+        {banners.map((banner, index) => (
+          <div key={index} className="banner__slide">
+            <img
+              src={banner.image}
+              alt={banner.title}
+              className="banner__image"
+            />
+            <div className="banner__contents">
+              <h1 className="banner__title">{banner.title}</h1>
+              <div className="banner__buttons">
+                <button
+                  className="banner__button"
+                  onClick={() => openSpotifyLink(banner.spotifyLink)}
+                >
+                  Listen on Spotify
+                </button>
+                <button
+                  className="banner__button"
+                  onClick={() =>
                     window.open(
-                      "https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX",
+                      "https://www.youtube.com/@MAMAMOO_OFFICIAL",
                       "_blank"
-                    ),
-                },
-                "Listen on Spotify"
-              ),
-              React.createElement(
-                "button",
-                {
-                  className: "banner__button",
-                  onClick: () =>
-                    window.open(
-                      "https://www.youtube.com/user/ibighit",
-                      "_blank"
-                    ),
-                },
-                "Watch on YouTube"
-              )
-            ),
-            React.createElement(
-              "p",
-              { className: "banner__description" },
-              banner.description
-            )
-          ),
-          React.createElement("div", { className: "banner__cover" })
-        )
-      )
-    )
+                    )
+                  }
+                >
+                  Watch on YouTube
+                </button>
+              </div>
+              <p className="banner__description">{banner.description}</p>
+            </div>
+            <div className="banner__cover"></div>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
-export default banner;
+export default Banner;
